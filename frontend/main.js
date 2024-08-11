901a0bd03f43c4eb6c9756e2a3b03881ff80e0de
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2024-08-11 19:44:09 +0200

    loading guitar into a scene

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
+import "./style.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 
@@ -9,22 +11,19 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 2;
+
+camera.position.set(-10, 2, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const loader = new GLTFLoader();
+
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
-
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const light = new THREE.AmbientLight(0x404040, 100);
+scene.add(light);
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
@@ -37,15 +36,27 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   controls.update();
 
   render();
 }
 
 function render() {
+  renderer.setClearColor(0x2b2b2b, 1);
   renderer.render(scene, camera);
 }
 animate();
+loader.load(
+  "public/guitar.gltf",
+  (gltf) => {
+    const guitar = gltf.scene;
+
+    guitar.position.set(0, -5, 0);
+
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
