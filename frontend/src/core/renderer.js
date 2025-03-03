10300f863133548977631f9db20e9f7c82bf5be4
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2025-03-03 19:51:58 +0100

    enabling antialiasing

--- frontend/src/core/renderer.js
+++ frontend/src/core/renderer.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 
 export const Renderer = () => {
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
